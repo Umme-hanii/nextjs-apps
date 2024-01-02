aa6cc5a418b87d76a3500aef4be2baebf7d4d871
Umme-hanii <ummehanimohammed9@gmail.com>
2024-01-03 00:27:30 +0530

    Static Images

--- app/drinks/[id]/page.js
+++ app/drinks/[id]/page.js
@@ -1,4 +1,7 @@
-import SingleDrink from "@/components/SingleDrink";
+import Link from "next/link";
+// import SingleDrink from "@/components/SingleDrink";
+import drinkImg from "./drink.jpg";
+import Image from "next/image";
 
 const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
 
@@ -14,7 +17,23 @@ const getSingleDrink = async (drinkId) => {
 const SingleDrinkPage = async ({ params }) => {
   const drink = await getSingleDrink(params.id);
   const { strDrink, strDrinkThumb } = drink;
-  return <SingleDrink drinkName={strDrink} drinkImage={strDrinkThumb} />;
+  // return <SingleDrink drinkName={strDrink} drinkImage={strDrinkThumb} />;
+
+  return (
+    <div>
+      <Link href="/drinks" className="btn btn-accent mb-4">
+        back to drinks
+      </Link>
+      <div className="mx-auto w-3/5">
+        <h3 className="font-bold mb-4">{strDrink}</h3>
+        <Image
+          src={drinkImg}
+          alt={strDrink}
+          className="w-48 h-48 border-4 border-accent rounded-lg"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default SingleDrinkPage;
