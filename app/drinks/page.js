a46b605326acadbffe440e0ab019e288e93ce169
Umme-hanii <ummehanimohammed9@gmail.com>
2024-01-02 20:09:00 +0530

    Fetch data in server component

--- app/drinks/page.js
+++ app/drinks/page.js
@@ -1,4 +1,9 @@
-const DrinksPage = () => {
+const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a";
+
+const DrinksPage = async () => {
+  const resp = await fetch(url);
+  const { drinks } = await resp.json();
+  console.log(drinks[0]);
   return <div>DrinksPage</div>;
 };
 export default DrinksPage;
