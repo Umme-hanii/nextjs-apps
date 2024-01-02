4f376015863db4c89d798c3b62f1e8bcb6b0a988
Umme-hanii <ummehanimohammed9@gmail.com>
2024-01-02 20:19:55 +0530

    loading and error components

--- app/drinks/page.js
+++ app/drinks/page.js
@@ -1,9 +1,20 @@
-const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a";
+const url = "https://www.thecocktaildb.com/api/json/v1/1/search.phps?f=a";
+
+const fetchDrinks = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const resp = await fetch(url);
+
+  if (!resp.ok) {
+    throw new Error("Failed to fetch drinks");
+  }
+  const data = await resp.json();
+  return data;
+};
 
 const DrinksPage = async () => {
-  const resp = await fetch(url);
-  const { drinks } = await resp.json();
+  const { drinks } = await fetchDrinks();
   console.log(drinks[0]);
   return <div>DrinksPage</div>;
 };
+
 export default DrinksPage;
