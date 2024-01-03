69a40244cf870d445efd552135b202dfa904f69b
Umme-hanii <ummehanimohammed9@gmail.com>
2024-01-03 21:14:21 +0530

    prisma CRUD practise

--- app/prisma-example/page.js
+++ app/prisma-example/page.js
@@ -1,4 +1,32 @@
-const PrismaPage = () => {
-  return <div>PrismaPage</div>;
+import prisma from "@/utils/db";
+
+const prismaHandlers = async () => {
+  await prisma.task.create({
+    data: {
+      content: "wake up",
+    },
+  });
+  const allTasks = await prisma.task.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  return allTasks;
+};
+
+const PrismaPage = async () => {
+  const tasks = await prismaHandlers();
+  return (
+    <div>
+      <h1 className="text-7xl">PrismaExample</h1>
+      {tasks.map((task) => {
+        return (
+          <h2 key={task.id} className="text-xl py-2">
+            😁 {task.content}
+          </h2>
+        );
+      })}
+    </div>
+  );
 };
 export default PrismaPage;
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const links = [
   { href: "/client", label: "client" },
   { href: "/drinks", label: "drinks" },
-  { href: "/query", label: "query" },
+  { href: "/prisma-example", label: "prisma" },
   { href: "/tasks", label: "tasks" },
 ];
 
