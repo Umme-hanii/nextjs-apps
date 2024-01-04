4ab317c9bb3ea31cd98cbcea5778e8f25ae0622f
Umme-hanii <ummehanimohammed9@gmail.com>
2024-01-04 23:26:29 +0530

    use 'zod' library to validate the form fields

--- utils/actions.js
+++ utils/actions.js
@@ -1,7 +1,9 @@
 "use server";
 import { revalidatePath } from "next/cache";
-import prisma from "./db";
 import { redirect } from "next/navigation";
+import { z } from "zod";
+
+import prisma from "./db";
 
 export const getAllTasks = async () => {
   return await prisma.task.findMany({
@@ -13,17 +15,26 @@ export const getAllTasks = async () => {
 
 export const createTask = async (prevState, formData) => {
   // await new Promise((resolve) => setTimeout(resolve, 2000));
+  const Task = z.object({
+    content: z.string().min(5),
+  });
+
   try {
     const content = formData.get("content");
+    Task.parse({
+      content,
+    });
+
     await prisma.task.create({
       data: {
-        content: content,
+        content,
       },
     });
+
     revalidatePath("/tasks");
     return { message: "Success!!!" };
   } catch (error) {
-    return { message: "error..." };
+    return { message: "error...." };
   }
 };
 
